abffd793224395ea41137fbf518c4f1c5aa37c8c
Robert Amoah <mr_robertamoah@yahoo.com>
2024-10-28 05:34:42 +0000

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-slate-900">
+        <header className="bg-slate-800 text-white text-center p-4">
+            <h1 className="text-2xl">Hello Vite + React!</h1>
+        </header>
+        <main className="p-4">
+            <p className="text-white">Edit src/App.tsx and save to test HMR updates.</p>
+            <p className="text-white">Learn Vite: <a href="https://vitejs.dev/" className="text-blue-300">Vite Docs</a></p>
+            <p className="text-white">Learn React: <a href="https://reactjs.org/" className="text-blue-300">React Docs</a></p>
+        </main>
+    </div>
+  );
+}
+
+export default App;
